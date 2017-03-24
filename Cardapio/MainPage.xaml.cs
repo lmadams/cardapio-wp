e7954fae774a84003001a1cd67ec7efdc1d8dcda
Lucas Adams <adams.lucass@gmail.com>
2017-03-23 21:23:06 -0300

    funcionalidade para abrir tela de sobremesas

--- Cardapio/MainPage.xaml.cs
+++ Cardapio/MainPage.xaml.cs
@@ -22,6 +22,11 @@ namespace Cardapio
             //BuildLocalizedApplicationBar();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Sobremesas.xaml", UriKind.Relative));
+        }
+
         // Sample code for building a localized ApplicationBar
         //private void BuildLocalizedApplicationBar()
         //{
